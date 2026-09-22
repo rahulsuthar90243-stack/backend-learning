b17943e1ddf0b282aae6d7bc44ce6c4e556fc0b6
Rahul <rahulsuthar90243@gmail.com>
2026-09-22 23:53:39 +0530

    mongoDB connection update

--- T_COMPLETE_BACKEND/src/db/db.js
+++ T_COMPLETE_BACKEND/src/db/db.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-
-async function connectDB(){
-    try {
-        if(!process.env.MONGODB_URL){
-            console.log("MONGODB_URL is not defiend in the environment");
-        }
-
-        const mongoURL = `${process.env.MONGODB_URL}/${process.env.DB_NAME}`;
-        await mongoose.connect(mongoURL);
-        console.log("MONGODB connection successfully");
-    } catch (error) {
-        console.log("MONGODB connection error: ", error);
-        throw error;
+async function connectDB() {
+  try {
+    if (!process.env.MONGODB_URL || !process.env.DB_NAME) {
+      throw new Error("MONGODB_URL or DB_NAME is not defined in the environment");
     }
 
+    const mongoURL = `${process.env.MONGODB_URL}/${process.env.DB_NAME}`;
+    await mongoose.connect(mongoURL);
+    console.log("MONGODB connection successfully");
+  } catch (error) {
+    console.log("MONGODB connection error:", error.message);
+    throw error;
+  }
 }
+
+export default connectDB;
