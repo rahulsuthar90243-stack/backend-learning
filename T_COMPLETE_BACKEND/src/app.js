7b22d0d456d0d141cf70ac0a3be808ef213d7486
Rahul <rahulsuthar90243@gmail.com>
2026-09-22 23:52:15 +0530

    data post in mongoDB  and get api create

--- T_COMPLETE_BACKEND/src/app.js
+++ T_COMPLETE_BACKEND/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import {uplodeFile} from "./services/storage.service.js";
+import { postModel } from "./models/post.model.js";
 
 
 const app = express();
@@ -13,10 +14,23 @@ app.post("/create-post",  uplode.single("image"), async(req, res) => {
   console.log(req.file);
 
   const result = await uplodeFile(req.file.buffer);
-  console.log(result);
+  
+  const post = await postModel.create({
+    image: result.url,
+    caption: req.body.caption
+  })
   
   res.status(200).json({
-     message: "Post create successfully"
+     message: "Post create successfully",
+     post
+  })
+})
+
+app.get("/create-post", async (req, res) => {
+  const data = await postModel.find();
+
+  res.status(200).json({
+    data
   })
 })
 
