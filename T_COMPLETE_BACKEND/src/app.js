83a87ad80d231623b30ccaf8956fd56d866b2803
Rahul <rahulsuthar90243@gmail.com>
2026-09-23 23:10:10 +0530

    CORS middleware for security system inplemented

--- T_COMPLETE_BACKEND/src/app.js
+++ T_COMPLETE_BACKEND/src/app.js
@@ -2,9 +2,11 @@ import express from "express";
 import multer from "multer";
 import {uplodeFile} from "./services/storage.service.js";
 import { postModel } from "./models/post.model.js";
+import cors from "cors"
 
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 const uplode = multer({storage: multer.memoryStorage()})
