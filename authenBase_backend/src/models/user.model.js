d9e77b3537b4b146756c779475d8bb24179d269e
Rahul <rahulsuthar90243@gmail.com>
2026-09-26 00:03:27 +0530

    email unique true change

--- authenBase_backend/src/models/user.model.js
+++ authenBase_backend/src/models/user.model.js
@@ -6,7 +6,8 @@ const userSchema = Schema(
             type: String
         },
         email: {
-            type: String
+            type: String,
+            unique: true
         },
         password: {
             type: String
