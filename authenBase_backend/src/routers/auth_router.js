c78537898d95e3b9f88ce92a10770a826d7e92de
Rahul <rahulsuthar90243@gmail.com>
2026-09-26 00:04:19 +0530

    add cookies storage

--- authenBase_backend/src/routers/auth_router.js
+++ authenBase_backend/src/routers/auth_router.js
@@ -4,6 +4,13 @@ import auth_controllers from "../controllers/auth_controllers.js";
 const router = express.Router();
 
 router.post("/register", auth_controllers.registerUser);
+router.get("/test", (req, res) => {
+    console.log("Cookie", req.cookies);
+    res.json({
+        message: "Test route",
+        Cookies: req.cookies
+    })
+})
 
 
 export default router
