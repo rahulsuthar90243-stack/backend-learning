4c0990e093850ff4c571a4c757ccafcae99c60a4
Rahul <rahulsuthar90243@gmail.com>
2026-09-26 00:06:39 +0530

    add user already exists condition

--- authenBase_backend/src/controllers/auth_controllers.js
+++ authenBase_backend/src/controllers/auth_controllers.js
@@ -7,18 +7,33 @@ async function registerUser(req, res) {
     try {
 
         const {username, email, password } = req.body;
+
+         const isUserAlreadyExists = await userModel.findOne({
+            email
+        })
+
+        if(isUserAlreadyExists){
+            return res.status(409).json({ 
+                message: "user already exists"
+            });
+        }
+        
+
         const user = await userModel.create({
             username, email, password
         })
+
+       
 
         const token = jwt.sign({
             id: user._id
         }, process.env.JWT_SECRET)
 
+        res.cookie("Token", token);
+
         res.status(200).json({
             message: "User register successfully",
-            user,
-            token
+            user
         })
         
     } catch (error) {
