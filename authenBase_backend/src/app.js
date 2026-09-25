877bdcbf963f566210e62a0039c408dffee1dc9d
Rahul <rahulsuthar90243@gmail.com>
2026-09-26 00:01:02 +0530

    post api create

--- authenBase_backend/src/app.js
+++ authenBase_backend/src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import auth_router from "./routers/auth_router.js"
+import cookieParser from "cookie-parser"
+import post_router from "./routers/post.router.js"
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", auth_router);
+app.use("/api/post", post_router)
 
 export default app;
